ef95d9868c2120099378130c536430dec647ed22
Matyáš Pešek <matyas.pesek@gmail.com>
2024-12-03 07:00:18 +0100

    brokolice seems to install

--- config/brokolice.nix
+++ config/brokolice.nix
@@ -1,9 +1,6 @@
 { inputs, ... }@flakeContext:
 inputs.nixpkgs.lib.nixosSystem {
   modules = [
-    #./brokolice/hardware.nix flakeContext
-    #/mnt/etc/nixos/hardware-configuration.nix
-    
     (if builtins.pathExists "/mnt/etc/nixos/hardware-configuration.nix"
     then /mnt/etc/nixos/hardware-configuration.nix
     else /etc/nixos/hardware-configuration.nix)
