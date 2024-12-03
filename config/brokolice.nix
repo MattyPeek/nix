72c800e99cb8611aa67845ec9b498743332b854a
Matyáš Pešek <matyas.pesek@gmail.com>
2024-12-03 06:58:43 +0100

    added brokolice 6

--- config/brokolice.nix
+++ config/brokolice.nix
@@ -1,12 +1,12 @@
 { inputs, ... }@flakeContext:
 inputs.nixpkgs.lib.nixosSystem {
   modules = [
-    ./brokolice/hardware.nix flakeContext
+    #./brokolice/hardware.nix flakeContext
     #/mnt/etc/nixos/hardware-configuration.nix
     
-    #(if builtins.pathExists "/mnt/etc/nixos/hardware-configuration.nix"
-    #then /mnt/etc/nixos/hardware-configuration.nix
-    #else /etc/nixos/hardware-configuration.nix)
+    (if builtins.pathExists "/mnt/etc/nixos/hardware-configuration.nix"
+    then /mnt/etc/nixos/hardware-configuration.nix
+    else /etc/nixos/hardware-configuration.nix)
 
     ./brokolice/system.nix flakeContext
     ./brokolice/packages.nix flakeContext
