c04cb3648527a6c6a32749badb59b01936d78e66
Matyáš Pešek <matyas.pesek@gmail.com>
2024-12-03 06:51:13 +0100

    added brokolice 6

--- config/brokolice.nix
+++ config/brokolice.nix
@@ -1,15 +1,15 @@
 { inputs, ... }@flakeContext:
 inputs.nix-darwin.lib.darwinSystem {
   modules = [
-    ./brokolice/hardware.nix
+    ./brokolice/hardware.nix flakeContext
     #/mnt/etc/nixos/hardware-configuration.nix
     
     #(if builtins.pathExists "/mnt/etc/nixos/hardware-configuration.nix"
     #then /mnt/etc/nixos/hardware-configuration.nix
     #else /etc/nixos/hardware-configuration.nix)
 
-    ./brokolice/system.nix
-    ./brokolice/packages.nix
+    ./brokolice/system.nix flakeContext
+    ./brokolice/packages.nix flakeContext
   ];
   system = "x86_64-linux";
 }
