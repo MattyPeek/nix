088b138eee5762e1249ee84290f54338c11d92da
Matyáš Pešek <matyas.pesek@gmail.com>
2025-03-31 15:02:18 +0200

    minor update

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     description = "MattyPeek's nix flake";
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/24.11";
-        nix-darwin.url = "github:LnL7/nix-darwin";
+        nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
         nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
         disko.url = "github:nix-community/disko";
         disko.inputs.nixpkgs.follows = "nixpkgs";
