67ce5b3346211fea9d08c78a34c45871475e8c74
Matyáš Pešek <matyas.pesek@gmail.com>
2025-04-09 00:58:25 +0200

    minor update

--- config/mcbp/packages.nix
+++ config/mcbp/packages.nix
@@ -111,43 +111,26 @@ in {
         };
         
         # Fix app aliases
-        system.activationScripts.applications.text = lib.mkForce ''
-            echo "setting up ~/Applications..." >&2
+        system.activationScripts.applications.text = let
+            user = "maty";
+            userApps = "${user}/Applications";
+            nixApps = "${userApps}/NixApps";
+            env = pkgs.buildEnv {
+                name = "system-applications";
+                paths = config.environment.systemPackages;
+                pathsToLink = "/Applications";
+            };
+        in pkgs.lib.mkForce ''
+            echo "setting up ${nixApps}..." >&2
+            rm -rf ${nixApps}
+            mkdir -p ${nixApps}
             
-            user="maty"
-            applications="/Users/$user/Applications"
-            nix_apps="$applications/Nix Apps"
-
-            # Needs to be writable by the user so that home-manager can symlink into it
-            if ! test -d "$applications"; then
-                echo "Creating $applications folder"
-                mkdir -p "$applications"
-                chown "$user": "$applications"
-                chmod u+w "$applications"
-            fi
-            echo breakpoint1;
-
-            # Delete the directory to remove old links
-            echo "Recreating $nix_apps..."
-            rm -rf "$nix_apps"
-            mkdir -p "$nix_apps"
-            echo "APPLICATIONS: ${config.system.build.applications}/Applications"
-            find ${config.system.build.applications}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+            find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
             while read -r src; do
-                echo "SRC: $src";
-                # Spotlight does not recognize symlinks, it will ignore directory we link to the applications folder.
-                # It does understand MacOS aliases though, a unique filesystem feature. Sadly they cannot be created
-                # from bash (as far as I know), so we use the oh-so-great Apple Script instead.
-                /usr/bin/osascript -e "
-                    set fileToAlias to POSIX file \"$src\"
-                    set applicationsFolder to POSIX file \"$nix_apps\"
-                    tell application \"Finder\"
-                    make alias file to fileToAlias at applicationsFolder
-                    # This renames the alias; 'mpv.app alias' -> 'mpv.app'
-                    set name of result to \"$(rev <<< "$src" | cut -d'/' -f1 | rev)\"
-                    end tell
-                " 1>/dev/null
-             done
+                app_name=$(basename "$src")
+                echo "copying $src" >&2
+                ${pkgs.mkalias}/bin/mkalias "$src" "${nixApps}$app_name"
+            done
         '';
     };
 }
