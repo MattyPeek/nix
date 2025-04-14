77a1679820b7476aa18875f0d82626a28be894ac
MattyPeek <matyas.pesek@gmail.com>
2025-04-14 16:09:53 +0200

    minor update

--- config/natalie/system.nix
+++ config/natalie/system.nix
@@ -34,7 +34,7 @@
     boot.extraModulePackages = [ ];
 
     # Plymouth
-    boot.plymouth.enable = true;
+    boot.plymouth.enable = false;
     boot.plymouth.logo = pkgs.fetchurl {
         url = "https://nixos.org/logo/nixos-hires.png";
         sha256 = "1ivzgd7iz0i06y36p8m5w48fd8pjqwxhdaavc0pxs7w1g7mcy5si";
