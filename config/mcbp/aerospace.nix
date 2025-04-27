5f16bc26efbb3687ba26a5a1e3fea4cd68bdf211
Matyáš Pešek <matyas.pesek@gmail.com>
2025-04-27 03:53:37 +0200

    minor update

--- config/mcbp/aerospace.nix
+++ config/mcbp/aerospace.nix
@@ -9,10 +9,13 @@ services.aerospace = {
             outer.right = 8;
         };
         mode.main.binding = {
+            # Move around
             alt-h = "focus left";
             alt-j = "focus down";
             alt-k = "focus up";
             alt-l = "focus right";
+            
+            # Switch workspace
             alt-1 = "workspace 1";
             alt-2 = "workspace 2";
             alt-3 = "workspace 3";
@@ -21,6 +24,10 @@ services.aerospace = {
             alt-6 = "workspace 6";
             alt-7 = "workspace 7";
             alt-8 = "workspace 8";
+
+            # Switch layout
+            alt-slash = "layout tiles horizontal vertical";
+            alt-comma = "layout accordion horizontal vertical";
         };
     };
 };
