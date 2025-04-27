669e932c399d1b1c991f27c4196366b9618a80df
Matyáš Pešek <matyas.pesek@gmail.com>
2025-04-27 04:27:41 +0200

    minor update

--- config/mcbp/aerospace.nix
+++ config/mcbp/aerospace.nix
@@ -64,13 +64,15 @@ services.aerospace = {
             alt-shift-f = [ "layout floating tiling" "mode main" ];
 
             # Launching
-            alt-enter = '''
+            alt-enter = {
+            '''
                 osascript -e '
                     tell application "iTerm"
                         create window with default profile
                         activate
                     end tell'
-            ''';
+            '''
+            };
         };
     };
 };
