5f4fce5d1ab2298fdc8084b73ff127459677845d
Matyas Pesek <matyas.pesek@gmail.com>
2025-05-09 13:59:02 +0200

    minor update

--- config/brokolice/zrepl.nix
+++ config/brokolice/zrepl.nix
@@ -40,10 +40,12 @@
     systemd.services.zrepl.serviceConfig.ExecStartPost = lib.mkAfter [
         ''
             /run/current-system/sw/bin/bash -c '
+                echo "Running chown script..."
                 for i in {1..10}; do
                     if compgen -G "/var/run/zrepl/stdinserver/*" > /dev/null; then
                         /run/current-system/sw/bin/chown :zrepl /var/run/zrepl/stdinserver/*
                         /run/current-system/sw/bin/chmod 0770 /var/run/zrepl/stdinserver/*
+                        echo "chown succeeded..."
                         exit 0
                     fi
                     sleep 0.5
