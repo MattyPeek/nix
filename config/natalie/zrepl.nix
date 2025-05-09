cb5db3feaa57833e1eb086bb9105765048659fc2
MattyPeek <matyas.pesek@gmail.com>
2025-05-09 10:43:18 +0200

    minor update

--- config/natalie/zrepl.nix
+++ config/natalie/zrepl.nix
@@ -1,31 +1,40 @@
 { config, lib, pkgs, disko, ... }: {
     services.zrepl = {
         enable = true;
-        settings = ''
-            global:
-              logging:
-                - type: syslog
-                  format: human
-                  level: warn
-            jobs:
-              - name: natalie-backup
-                type: push
-                connect:
-                  type: ssh+stdinserver
-                  host: brokolice.pesek.pro
-                  user: maty
-                  port: 412
-                  identity_file: /etc/zrepl/ssh/id_ed25519
-                  client_identity: pushclient
-                  options:
-                    - "Compression=yes"
-                filesystems: {
-                  "pool1/system": true,
+        settings = {
+            global = {
+                logging = [
+                {
+                    type = "syslog";
+                    format = "human";
+                    level = "warn";
                 }
-                snapshotting:
-                  type: periodic
-                  prefix: zrepl_
-                  interval: 300s
-        '';
+                ];
+            };
+            jobs = [
+            {
+                name = "natalie-backup";
+                type = "push";
+                connect = {
+                    type = "ssh+stdinserver";
+                    host = "brokolice.pesek.pro";
+                    user = "maty";
+                    port = 412;
+                    identity_file = "/etc/zrepl/ssh/id_ed25519";
+                    client_identity = "pushclient";
+                    options = [ "Compression=yes" ];
+                };
+                filesystems = {
+                    "pool1/system" = true;
+                };
+                snapshotting = {
+                    type = "periodic";
+                    prefix = "zrepl_";
+                    interval = "300s";
+                };
+            }
+            ];
+        };
+
     };
-}
+                                   }
