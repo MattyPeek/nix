d48458042c22cbd6fe433c3fb99afbf3d881c3c1
Matyas Pesek <matyas.pesek@gmail.com>
2025-05-18 12:58:49 +0200

    minor update

--- config/brokolice/games.nix
+++ config/brokolice/games.nix
@@ -1,5 +1,10 @@
 { config, lib, pkgs, ... }: {
     services.openarena = {
         enable = true;
+        extraFlags = [
+            "+set dedicated 2"
+            "+set sv_hostname 'NixOS OpenArena Server'"
+            "+map oa_dm1"
+        ];
     }
 }
