72c911b62e66ae51519d5e93b50bd40f1ae460a6
MattyPeek <matyas.pesek@gmail.com>
2025-06-24 18:58:22 +0200

    minor update

--- flake.nix
+++ flake.nix
@@ -46,7 +46,8 @@
 	                ./config/natalie/firewall.nix
 	                ./config/natalie/wireguard.nix
 	                ./config/natalie/desktop.nix
-	                ./config/natalie/zrepl.nix
+	                #./config/natalie/zrepl-receive.nix
+	                ./config/natalie/zrepl-send.nix
 	                #./config/natalie/games.nix
                 ];
             };
