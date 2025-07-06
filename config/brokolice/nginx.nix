c3d2e65046d7730df4bdb1dd488df448c6524df7
Matyas Pesek <matyas.pesek@gmail.com>
2025-07-06 06:31:38 +0200

    minor update

--- config/brokolice/nginx.nix
+++ config/brokolice/nginx.nix
@@ -114,6 +114,19 @@
                     '';
                 };
             };
+            "pass.pesek.pro" = {
+                enableACME = true;
+                forceSSL = true;
+                locations."/" = {
+                    proxyPass = "http://127.0.0.1:6908";
+                    extraConfig = ''
+                        proxy_set_header Host $host;
+                        proxy_set_header X-Real-IP $remote_addr;
+                        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                        proxy_set_header X-Forwarded-Proto $scheme;
+                    '';
+                };
+            };
         };
     };
 }
