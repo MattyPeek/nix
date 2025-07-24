0612a7559d4620fd4f0aee245fc81da80bf2dd2c
Matyas Pesek <matyas.pesek@gmail.com>
2025-07-24 14:01:16 +0200

    minor update

--- config/brokolice/cron.nix
+++ config/brokolice/cron.nix
@@ -2,12 +2,7 @@
     services.cron = {
         enable = true;
         systemCronJobs = [
-        {
-            #minute = "*/10";
-            #user = "root";
-            #command = "/srv/monitoring/home-internet-check.py";
             "*/10 * * * * root /srv/monitoring/home-internet-check.py"
-        }
         ];
     };
 }
